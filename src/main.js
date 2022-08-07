861f39770312478c71b785edeb0f2d4c8f6732b1
zyrif <rubaiyat9@gmail.com>
2022-08-07 06:53:28 +0600

    Create and add Axios instance to the Vue instance

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import Axios from "axios";
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$axios = Axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+  timeout: 1000
+})
+
 
 new Vue({
   vuetify,
